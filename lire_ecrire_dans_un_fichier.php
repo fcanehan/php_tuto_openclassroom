81e071dd3b95c90d63db1a52f66781c4f7f906f5
fabien <fabien@beweb-16.176N>
2018-04-25 13:49:57 +0200

    exo lire_ecrire_un_fichier

--- lire_ecrire_dans_un_fichier.php
+++ lire_ecrire_dans_un_fichier.php
@@ -6,8 +6,8 @@ $monfichier = fopen('compteur.txt', 'r+');
 // lecture et stockage de la ligne du fichier
 //$ligne = fgets($monfichier);
 
-$pagesvues = fgets($monofichier);   // lecture de la 1ère ligne
-$pagesvues +=5;                     // on augmente de 1 le nb de fois où la page a été lue
+$pagesvues = fgets($monfichier);   // lecture de la 1ère ligne
+$pagesvues +=1;                     // on augmente de 1 le nb de fois où la page a été lue
 
 fseek($monfichier, 0);              // réintialisation du curseur au début du fichier
 
